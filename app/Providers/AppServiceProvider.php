679910b03f31b089ae276d80f11b090cf566c193
ihwankho <ihwankho@gmail.com>
2026-06-12 22:15:38 +0700

    Implement HTTPS redirection in AppServiceProvider for production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || env('APP_URL') !== 'http://localhost') {
+            URL::forceScheme('https');
+
+            // Mengambil base URL langsung dari APP_URL di .env Anda
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
